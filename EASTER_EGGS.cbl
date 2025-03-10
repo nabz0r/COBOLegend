ec8f2569b7ceb5e953012a95f9b03219b84b4104
nabilux <nabz0r@gmail.com>
2025-03-10 11:44:48 +0100

    Update EASTER_EGGS.cbl

--- EASTER_EGGS.cbl
+++ EASTER_EGGS.cbl
@@ -6,7 +6,7 @@
       *****************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. EASTER-EGGS.
-       AUTHOR. CLAUDE.
+       AUTHOR. NABZ0R.
        DATE-WRITTEN. 2025-03-04.
        SECURITY. TOP-SECRET.
       
